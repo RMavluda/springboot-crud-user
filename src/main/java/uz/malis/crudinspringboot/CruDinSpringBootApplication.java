e7ec3bcb8a6a12bf61717b5b17c325ed1e9cdd24
Mavluda <rahimqulimavluda@gmail.com>
2024-04-08 09:55:06 +0500

    commited

---
+++ src/main/java/uz/malis/crudinspringboot/CruDinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uz.malis.crudinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDinSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CruDinSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/uz/malis/crudinspringboot/CruDinSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.malis.crudinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDinSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
